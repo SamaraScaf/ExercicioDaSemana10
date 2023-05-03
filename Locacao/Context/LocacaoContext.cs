8c5a74ced8d27245ea6306397b9416916e86b5ad
SamaraScaf <122405994+SamaraScaf@users.noreply.github.com>
2023-05-03 13:53:42 -0300

    ExercicioDaSemana10

---
+++ Locacao/Context/LocacaoContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Locacao.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Locacao.Context
+{
+    public class LocacaoContext:DbContext
+    {
+        public DbSet <CarroModel> Carros { get; set; }
+        public DbSet<MarcaModel> Marcas { get; set; }
+
+        public LocacaoContext() { }
+        public LocacaoContext(DbContextOptions<LocacaoContext> optionsBuider):base(optionsBuider)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
